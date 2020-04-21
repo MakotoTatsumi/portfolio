bdd029eb78201f7547645fb9f0242cff494d2490
辰己 誠 <m_tatsumi@waku-2.com>
2020-04-21 18:50:45 +0900

    ポートフォリオ作成

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="markdown">
+    <div class="sideBar">
+      <div class="sideBar__title">makofolio</div>
+      <div class="sideBar__sns">Twitter:<a href="">@makoto</a></div>
+      <div class="sideBar__sns">GitHub:<a href="">@makoto</a></div>
+      <div class="sideBar__sns">Qita:<a href="">@makoto</a></div>
+    </div>
+    <div class="contents">
+      <div v-html="compiledMarkdownText" />
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+import md from './portfolio.md'
+
+export default {
+  data () {
+    return {
+      markdownText: md.source    // sourceで Rawデータが取得できます 
+    }
+  },
+  computed: {
+    compiledMarkdownText: function () {
+      return marked(this.markdownText)    // computedにする必要あるかどうかは知らん
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.markdown {
+  display: flex;
+  height: 100vh;
+}
+
+.sideBar {
+  background-color: #6a9fb5;
+  width: 18rem;
+}
+
+.sideBar__title {
+  color: #fff;
+  font-size: 40px;
+  font-weight: bold;
+  padding: 280px 20px 40px 20px;
+}
+
+.sideBar__sns {
+  font-size: 16px;
+  padding-left: 20px;
+}
+
+.contents {
+  overflow: scroll;
+  padding: 10px;
+  width: 100%;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+
+module.exports = {
+  entry: {
+    'js/index': './src/index.js',
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist/src/'),
+    filename: '[name].js',
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, './src'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.md$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'vue-loader',
+          {
+            loader: 'markdown-to-vue-loader',
+            options: {
+              exportSource: true, // この設定でMarkdownのRawデータを読み込めるようにする
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // Babel のオプションを指定する
+        options: {
+          presets: [
+            // プリセットを指定することで、ES2020 を ES5 に変換
+            '@babel/preset-env',
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ]
+      },
+    ]
+  },
+  // import 文で .ts ファイルを解決するため
+  resolve: {
+    // Webpackで利用するときの設定
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    // Vueを読み込めるようにするため
+    new VueLoaderPlugin(),
+  ],
+};
